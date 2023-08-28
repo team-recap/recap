186f9d656af1a88954e7d21c34aa6dd4f21d25e4
KimJuWan <wndhks00000@naver.com>
2023-08-28 22:03:20 +0900

    Summarizer, Extractor, Graph 일부 수정, Short 삭제 요약하는 문장의 개수를 일차함수 형식으로 변경, 부사 제거 하는 부분 따로 함수화, 일부 변수명 수정

--- src/main/java/org/recap/sentence/Extractor.java
+++ src/main/java/org/recap/sentence/Extractor.java
@@ -22,37 +22,6 @@ public class Extractor {
             Sentence analyzedSentence = tagger.tagSentence(sentence);
             // analyzedSentence = parser.analyze(analyzedSentence); // 사용시 형태소를 잘 끊어주는 반면 속도가 느려짐
 
-            StringBuilder withOutMAGSentence = new StringBuilder();
-
-            int num=0;
-
-            // 문장 내 단어별
-            for (Word word : analyzedSentence) {
-                //System.out.println(analyzedSentence.getSize());
-                num++;
-
-                // 단어 내 형태소별 (ex. '리캡은' -> '리캡' + '은')
-                boolean hasMAG = false; // 부사 여부 확인
-                for (Morpheme morpheme : word) {
-                    //System.out.println(morpheme);
-                    if (morpheme.getTag().toString().equals("MAG")) {
-                        hasMAG = true;
-                        //System.out.println(morpheme);
-                    }
-                }
-
-                // 부사가 없는 경우에만 추가
-                if (!hasMAG) {
-                    withOutMAGSentence.append(word.getSurface());
-                    if(num<analyzedSentence.getSize()-1){
-                        withOutMAGSentence.append(" ");
-                    }
-                }
-            }
-            //System.out.println(withOutMAGSentence);
-
-            //////////////////////////////////////////////
-
             List<String> collectedWords = new ArrayList<>();
 
             // 문장 내 단어별
@@ -71,9 +40,40 @@ public class Extractor {
                     collectedWords.add(collectedWord);
             }
 
-            wordsWithSentences.put(withOutMAGSentence.toString(), collectedWords);
+            wordsWithSentences.put(makeMagRemovedSentence(analyzedSentence), collectedWords);
         }
 
         return wordsWithSentences;
+    }
+
+    public static String makeMagRemovedSentence(Sentence analyzedSentence) {
+
+        StringBuilder magRemovedSentence = new StringBuilder();
+
+        int lastSpaceChecker=0;
+
+        // 문장 내 단어별
+        for (Word word : analyzedSentence) {
+
+            lastSpaceChecker++;
+
+            // 단어 내 형태소별 (ex. '리캡은' -> '리캡' + '은')
+            boolean hasMAG = false; // 부사 여부 확인
+            for (Morpheme morpheme : word) {
+                if (morpheme.getTag().toString().equals("MAG")) {
+                    hasMAG = true;
+                }
+            }
+
+            // 부사가 없는 경우에만 추가
+            if (!hasMAG) {
+                magRemovedSentence.append(word.getSurface());
+                if(lastSpaceChecker < analyzedSentence.getSize()-1){
+                    magRemovedSentence.append(" ");
+                }
+            }
+        }
+
+        return magRemovedSentence.toString();
     }
 }
