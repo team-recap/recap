e7257dcab5b7df61f0d59ff2513c29d20e7e1089
Juwan-Kim <141032622+Juwan-Kim@users.noreply.github.com>
2023-08-28 22:11:25 +0900

    Revert "Summarizer, Extractor, Graph 일부 수정, Short 삭제"

--- src/main/java/org/recap/Summarizer.java
+++ src/main/java/org/recap/Summarizer.java
@@ -1,6 +1,7 @@
 package org.recap;
 
 import org.recap.sentence.Extractor;
+import org.recap.sentence.Short;
 import org.recap.sentence.Splitter;
 
 import java.util.List;
@@ -11,15 +12,21 @@ public class Summarizer {
     public List<String> summarize(String text, Graph.SimilarityMethods similarityMethods) {
         List<String> sentences = Splitter.split(text); // 문장 분리
 
-        int sentencesSize = sentences.size();
-        int recapSize = Math.round((1/20f)*sentencesSize+2); // 몇줄로 줄일 건지
-        //함수식 반올림으로 : y = (1/20)x + 2  //Desmos 그래프 복붙하면 : y=\frac{1}{20}x+2
-        //limit가 없으면 안들고 오기 때문에 값이 더 커도 문제없음. //값을 딱 맞춰야 되면 올림으로 : y=x^{0.3}+\frac{1}{50}x-0.2
+        //System.out.println(sentences.size());
 
-        Map<String, List<String>> wordsWithSentences = Extractor.extract(sentences); // 명사추출, 문장 내 부사제거
+        int sentenceSize = sentences.size();
 
-        // 그래프 생성
-        Graph graph = new Graph(wordsWithSentences, similarityMethods); // 그래프 및 가중치 생성
-        return graph.calculatePageRank(recapSize).stream().map(Map.Entry::getKey).collect(Collectors.toList());
+        if(sentenceSize<3){  //총문장 갯수가 3보다 작으면
+            return sentences;
+        }
+        else{
+            //List<String> withOutMAG_Sentences = Short.sentenceShorter(sentences); // MAG(부사제거)
+
+            Map<String, List<String>> wordsWithSentences = Extractor.extract(sentences); // 부사제거 명사추출 한번에
+
+            // 그래프 생성
+            Graph graph = new Graph(wordsWithSentences, similarityMethods);
+            return graph.calculatePageRank(Math.round(sentenceSize/2f)).stream().map(Map.Entry::getKey).collect(Collectors.toList());
+        }
     }
 }
--- src/main/java/org/recap/sentence/Short.java
+++ src/main/java/org/recap/sentence/Short.java
@@ -3,22 +3,22 @@ package org.recap.sentence;
 import kr.bydelta.koala.data.Morpheme;
 import kr.bydelta.koala.data.Sentence;
 import kr.bydelta.koala.data.Word;
-import kr.bydelta.koala.hnn.Parser;
+//import kr.bydelta.koala.hnn.Parser;
 import kr.bydelta.koala.hnn.Tagger;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
-public class Extractor {
-    public static Map<String, List<String>> extract(List<String> sentences) {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Short {
+    public static List<String> sentenceShorter(List<String> sentences) {
         final Tagger tagger = new Tagger();
         //final Parser parser = new Parser();
-        final Map<String, List<String>> wordsWithSentences = new LinkedHashMap<>(); // 키 - 문장, 값 - 추출된 단어들
+        List<String> withOutMAGSentences = new ArrayList<>();
 
         // 문장별
         for (String sentence : sentences) {
+
             Sentence analyzedSentence = tagger.tagSentence(sentence);
             // analyzedSentence = parser.analyze(analyzedSentence); // 사용시 형태소를 잘 끊어주는 반면 속도가 느려짐
 
@@ -47,33 +47,13 @@ public class Extractor {
                     if(num<analyzedSentence.getSize()-1){
                         withOutMAGSentence.append(" ");
                     }
+
                 }
             }
             //System.out.println(withOutMAGSentence);
-
-            //////////////////////////////////////////////
-
-            List<String> collectedWords = new ArrayList<>();
-
-            // 문장 내 단어별
-            for (Word word : analyzedSentence.getNouns()) {
-                String collectedWord = ""; // 저장될 단어
-
-                // 단어 내 형태소별 (ex. '리캡은' -> '리캡' + '은')
-                for (Morpheme morpheme : word) {
-                    // 모든 명사 종류 추출
-                    if (morpheme.getTag().toString().startsWith("N")) { // 명사 추출
-                        collectedWord = collectedWord.concat(morpheme.getSurface().replaceAll("[\".,“”]", "")); // 특수문자 삭제
-                    }
-                }
-
-                if (collectedWord.length() >= 2) // 단어 길이가 2 이상일 때만 해당 단어 저장
-                    collectedWords.add(collectedWord);
-            }
-
-            wordsWithSentences.put(withOutMAGSentence.toString(), collectedWords);
+            withOutMAGSentences.add(withOutMAGSentence.toString());
         }
 
-        return wordsWithSentences;
+        return withOutMAGSentences;
     }
 }
