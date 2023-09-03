13026e9291c4a7d00a3ad511443affe08de1db8f
ypjun100 <ypjun100@gmail.com>
2023-09-03 12:02:39 +0900

    fix: cosine similarity 계산 방법 수정

--- src/main/java/org/recap/sentence/Similarity.java
+++ src/main/java/org/recap/sentence/Similarity.java
@@ -14,18 +14,28 @@ public class Similarity {
 
     // 두 리스트의 단어 교집합 추출
     private static List<String> intersection(List<String> source, List<String> target) {
-        List<String> list = new ArrayList<>();
-        for (String word : source)
-            if (target.contains(word))
-                list.add(word);
-        return list;
+        Set<String> sourceSet = new HashSet<>(source);
+        Set<String> targetSet = new HashSet<>(target);
+        sourceSet.retainAll(targetSet); // sourceSet에 targetSet의 원소만 남김
+        return new ArrayList<>(sourceSet);
     }
 
 
     // 코사인 유사도 계산
     public static float calculateCosineSimilarity(Map.Entry<String, List<String>> entrySource, Map.Entry<String, List<String>> entryTarget) {
         List<String> intersection = intersection(entrySource.getValue(), entryTarget.getValue());
-        return (float) intersection.size() / (float) (Math.sqrt(entrySource.getValue().size()) * Math.sqrt(entryTarget.getValue().size()));
+
+        // 분자: 교집합의 크기
+        float numerator = intersection.size();
+
+        // 분모: 두 리스트의 크기의 제곱근 곱
+        float denominator = (float) (Math.sqrt(entrySource.getValue().size()) * Math.sqrt(entryTarget.getValue().size()));
+
+        // 분모가 0인 경우
+        if (denominator == 0f)
+            return 0f; // 분모가 0이면 코사인 유사도는 0
+
+        return numerator / denominator;
     }
 
     // 자카드 유사도 계산
